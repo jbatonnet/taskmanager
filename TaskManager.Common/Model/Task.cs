1557f3544fd476314379cf61b7d6430fba759b70
Julien Batonnet <julien.batonnet@gmail.com>
2015-11-09 14:21:31 +0100

    Basic TaskManager structure

---
+++ TaskManager.Common/Model/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Common
+{
+    public class Task
+    {
+        public int Id { get; private set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public List<Attachement> Attachements { get; set; }
+    }
+}
---
+++ TaskManager.Desktop/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskManager.Desktop
+{
+    public partial class MainWindow : Window
+    {
+        public DelegateCommand NewStorageCommand { get; private set; }
+        public DelegateCommand OpenStorageCommand { get; private set; }
+
+        public DelegateCommand UndoCommand { get; private set; }
+        public DelegateCommand RedoCommand { get; private set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
